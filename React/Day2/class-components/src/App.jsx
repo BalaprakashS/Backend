64d4dfc36936ca39e3abcdc9db12d555ca26eecb
BalaprakashS <balaprakash2901@gmail.com>
2026-09-22 18:20:14 +0530

    updated

---
+++ React/Day2/class-components/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Product from "./components/Product";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Profile />
+            <Product />
+            <About />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ React/Day2/class-components/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Profile extends Component {
+    render() {
+        return <h2>Profile Component</h2>;
+    }
+}
+
+export default Profile;
